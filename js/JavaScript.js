507ae47cdb05f8cfac680d1608772071c981f11d
Dmytro <herasymdmytro21@gmail.com>
2022-07-07 23:43:04 +0300

    big update

--- js/JavaScript.js
+++ js/JavaScript.js
@@ -1,26 +1,2 @@
-const surfaceAreaCalculator = (number) => {
-   return 4 * 3.14 * number * number;
-}
 
-const surfaceOfMars = surfaceAreaCalculator(1043430);
-
-const dimatest = surfaceAreaCalculator(82);
-console.log(surfaceOfMars)
-console.log(dimatest)
-// --------Я делал попытки---------------------
-const yourAge = (number) => {
-   return 100 - 50 + number;
-};
-const dimaAge = yourAge(22);
-
-console.log(dimaAge)
-console.log(dimaAge)
-
-
-const percentaCalculator = (number, total) => {
-   return number * 100 / total;
-}
-const winterDone = percentaCalculator(2, 3);
-console.log(percentaCalculator(5, 28));
-
-// ----------------------------
+console.log("Hello Pasha")
