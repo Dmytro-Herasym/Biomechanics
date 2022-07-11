f8ada04346be065e04ce1ea0ea3412525f43779a
Dmytro <herasymdmytro21@gmail.com>
2022-07-11 16:44:57 +0300

    add burger menu

--- js/JavaScript.js
+++ js/JavaScript.js
@@ -1,2 +1,49 @@
+// const isMobile = {
+// 	Android: function() {
+// 		return navigator.userAgent.match(/Android/i);
+// 	},
+// 	BlackBerry: function() {
+// 		return navigator.userAgent.match(/BlackBerry/i);
+// 	},
+// 	iOS: function() {
+// 		return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+// 	},
+// 	Opera: function() {
+// 		return navigator.userAgent.match(/Opera Mini/i);
+// 	},
+// 	Windows: function() {
+// 		return navigator.userAgent.match(/IEMobile/i) || navigator.userAgent.match(/WPDesktop/i);
+// 	},
+// 	any: function() {
+// 		return (
+// 			isMobile.Android() ||
+// 			isMobile.BlackBerry() ||
+// 			isMobile.iOS() ||
+// 			isMobile.Opera() ||
+// 			isMobile.Windows());
+// 	}
+// };
 
-console.log("Hello Pasha")
+// if (isMobile.any()) {
+// 	document.body.classList.add('_touch')
+// } else {
+// 	document.body.classList.add('_pc')
+// }
+
+
+
+
+
+
+
+
+const iconMenu = document.querySelector('.menu__icon'); 
+if (iconMenu) {
+	const menuBody = document.querySelector('.header__nav'); 
+	iconMenu.addEventListener("click", (e) => {
+		document.body.classList.toggle('_lock')
+		iconMenu.classList.toggle('_active')
+		menuBody.classList.toggle('_active')
+	});
+}
+
