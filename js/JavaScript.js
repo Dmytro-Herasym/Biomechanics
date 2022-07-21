a6a259cf54f80eedcf24d0f21aba190a4530caf3
Dmytro <herasymdmytro21@gmail.com>
2022-07-21 17:35:58 +0300

    fix burger

--- js/JavaScript.js
+++ js/JavaScript.js
@@ -1,42 +1,3 @@
-// const isMobile = {
-// 	Android: function() {
-// 		return navigator.userAgent.match(/Android/i);
-// 	},
-// 	BlackBerry: function() {
-// 		return navigator.userAgent.match(/BlackBerry/i);
-// 	},
-// 	iOS: function() {
-// 		return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-// 	},
-// 	Opera: function() {
-// 		return navigator.userAgent.match(/Opera Mini/i);
-// 	},
-// 	Windows: function() {
-// 		return navigator.userAgent.match(/IEMobile/i) || navigator.userAgent.match(/WPDesktop/i);
-// 	},
-// 	any: function() {
-// 		return (
-// 			isMobile.Android() ||
-// 			isMobile.BlackBerry() ||
-// 			isMobile.iOS() ||
-// 			isMobile.Opera() ||
-// 			isMobile.Windows());
-// 	}
-// };
-
-// if (isMobile.any()) {
-// 	document.body.classList.add('_touch')
-// } else {
-// 	document.body.classList.add('_pc')
-// }
-
-
-
-
-
-
-
-
 const iconMenu = document.querySelector('.menu__icon'); 
 if (iconMenu) {
 	const menuBody = document.querySelector('.header__nav'); 
@@ -45,5 +6,8 @@ if (iconMenu) {
 		iconMenu.classList.toggle('_active')
 		menuBody.classList.toggle('_active')
 	});
+	console.log(menuBody)
+	console.log(iconMenu)
 }
+
 
